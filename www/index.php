fd6e3a8a675b5e513f32ba771dded6bd25f70931
Alix <dehaut.alix@gmail.com>
2019-01-21 23:33:05 +0100

    start Rooting && add .htacces file

--- www/index.php
+++ www/index.php
@@ -1,3 +1,47 @@
 <?php
+require "conf.inc.php";
+
+function myAutoloader($class){
+    $classPath = "core/".$class.".class.php";
+    $classModel = "models/".$class.".class.php";
+    if(file_exists($classPath)){
+        include $classPath;
+    }else if(file_exists($classModel)){
+        include $classModel;
+    }
+}
+
+spl_autoload_register("myAutoloader");
+
+$slug = $_SERVER["REQUEST_URI"];
+
+//pour palier les paramètres GET
+$slugExploded = explode("?", $slug);
+$slug = $slugExploded[0];
+
+$routes = Routing::getRoute($slug);
+extract($routes);
+
+
+//vérifier l'existence du fichier et de la class controller
+if( file_exists($controllerPath) ){
+    include $controllerPath;
+    if( class_exists($controller)){
+        //instancier dynamiquement le controller
+        $controllerObject = new $controller();
+        //vérifier que la méthode (l'action) existe
+        if( method_exists($controllerObject, $methodAction) ){
+            //appel dynamique de la méthode
+            $controllerObject->$methodAction();
+        }else{
+            die("La methode ".$methodAction." n'existe pas");
+        }
+
+    }else{
+        die("La class controller ".$controller." n'existe pas");
+    }
+}else{
+    die("Le fichier controller ".$controller." n'existe pas");
+}
 
 print "Coucou projet de l'enfer";
