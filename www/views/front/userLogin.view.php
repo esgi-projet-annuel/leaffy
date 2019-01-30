1a4c7b27404d061d06576bb9dd3ace2e114731ba
Fabien Culot <fab.culot@gmail.com>
2019-01-30 21:11:27 +0100

    add formulaire login && mkdir js && modif css

--- www/views/front/userLogin.view.php
+++ www/views/front/userLogin.view.php
@@ -1,1 +1,27 @@
-<p>login user </p>
+<div class="container">
+  <div class="card card-login card card-login mx-auto mt-5">
+    <div class="card-header">Connexion</div>
+    <div class="card-body">
+      <form method="post">
+        <div class="form-login">
+          <div class="form-label-login">
+            <label for="inputEmail">Email</label>
+            <input type="email" id="inputEmail" class="form-control-login" placeholder="Email address" required="required" autofocus="autofocus" name="inputEmail">
+          </div>
+        </div>
+        <div class="form-login">
+          <div class="form-label-login">
+            <label for="inputPassword">Mot de passe</label>
+            <input type="password" id="inputPassword" class="form-control-login" placeholder="Password" required="required" name="inputPassword">
+          </div>
+        </div>
+
+        <input type="submit" class="form-control submit-button" value="Connexion">
+      </form>
+      <div class="text-center">
+        <a class="small" href="">Créer un compte</a>
+        <a class="small" href="">Mot de passe oublié ?</a>
+      </div>
+    </div>
+  </div>
+</div>
--- www/views/templates/front.tpl.php
+++ www/views/templates/front.tpl.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 
-<html lang="en">
+<html lang="fr">
 
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="../../public/css/style.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        <script src="../../public/js/jquery-3.3.1.min.js"></script>
+        <script src="../../public/js/script.js"></script>
         <title></title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <!--        TODO importer description du professionnel-->
         <meta name="description" content="">
 <!--        TODO importer nom du pro-->
