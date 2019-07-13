fa7603c5455fca7ed82ebec0c1938441d7911bc1
Fabien Culot <fab.culot@gmail.com>
2019-07-13 15:10:53 +0200

    Templating form forgetpassword

--- www/views/front/forgottenPassword.view.php
+++ www/views/front/forgottenPassword.view.php
@@ -1,19 +1,28 @@
 <?php $this->addHeader("header", "front")?>
+<main>
+  <div class="container">
+    <div class="card card-login card card-login mx-auto mt-5">
+      <div class="card-header">Mot de passe oublié ?</div>
+      <div class="card-body">
+          <p>Saisissez votre email ci-dessous. Nous vous enverrons un lien pour réinitialiser votre mot de passe.</p>
 
-
-<h2>Mot de passe oublié?</h2>
-<p>Vous pouvez le réinitialiser ici!</p>
-
-<?php
-    if(isset($message) && !empty($message)){
-    echo "<p>".$message. "</p>";
-}
-?>
-<div>
-    <form method="POST" action="<?php echo \LeaffyMvc\Core\Routing::getSlug('User','sendMailToResetPassword')?>">
-        <label for="email">Email:</label>
-        <input type="text" id="email" name="email">
-        <input type="submit" value="Envoyer">
-    </form>
-
-</div>
+        <?php
+            if(isset($message) && !empty($message)){
+            echo "<p>".$message. "</p>";
+        }
+        ?>
+        <div>
+            <form method="POST" action="<?php echo \LeaffyMvc\Core\Routing::getSlug('User','sendMailToResetPassword')?>">
+              <div class="form-login">
+                <label for="email">Email:</label>
+                <input class="form-control-login" type="text" id="email" name="email">
+              </div>
+              <div class="d-flex justify-content-around">
+                <input class="form-control button-back button-back--add" type="submit" value="Envoyer">
+              </div>
+            </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
--- www/views/front/resetPassword.view.php
+++ www/views/front/resetPassword.view.php
@@ -3,21 +3,31 @@ $user = new \LeaffyMvc\Models\User();
 $user->findOneObjectBy(['email'=>$_GET['email']]);
 
 $resetForm =<<<EOF
-  <p>Veuillez saisir votre nouveau mot de passe</p>
-  <form method="post" action="">
-    <div>
-        <label for="pass">Password:</label>
-        <input type="password" id="password" name="password" required>
-    </div>
+<main>
+  <div class="container">
+    <div class="card card-login card card-login mx-auto mt-5">
+      <div class="card-header">Mot de passe oublié ?</div>
+      <div class="card-body">
+        <p>Veuillez saisir votre nouveau mot de passe</p>
+        <form method="post" action="">
+          <div class="form-login">
+              <label for="pass">Password:</label>
+              <input class="form-control-login" type="password" id="password" name="password" required>
+          </div>
 
-    <div>
-        <label for="pass">Confirmation:</label>
-        <input type="password" id="confirmation" name="confirmation" required>
+          <div class="form-login">
+              <label for="pass">Confirmation:</label>
+              <input class="form-control-login" type="password" id="confirmation" name="confirmation" required>
+          </div>
+          <div class="d-flex justify-content-around">
+            <input type="hidden" value="$user->id">
+            <input class="form-control button-back button-back--add" type="submit" id="userId" name="userId" value="Valider">
+          </div>
+        </form>
+      </div>
     </div>
-    <input type="hidden" value="$user->id">
-
-    <input type="submit" id="userId" name="userId" value="Valider">
-  </form>
+  </div>
+</main>
 EOF;
 
 if(!empty($user)) {
@@ -28,8 +38,19 @@ if(!empty($user)) {
         echo $resetForm;
     }
     else {
-        echo "Erreur ! Le mot de passe ne peut etre changé...";
+        echo "
+        <main>
+          <div class='container'>
+            <div class='container'>
+              <div class='row'>
+                <div class='col-12'>
+                  <p class='validation'>Erreur ! Le mot de passe ne peut pas être changé...</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        ";
     }
 
 ?>
-
